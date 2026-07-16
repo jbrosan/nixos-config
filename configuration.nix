3a1e380667e0615082ee37ae7f30c46ddc5b1477
John Brosan <john.brosan@gmail.com>
2026-07-15 21:04:07 -0400

    Add Git to Framework 13 system packages

--- configuration.nix
+++ configuration.nix
@@ -111,6 +111,7 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+   git
    zed-editor
    cascadia-code
    jetbrains.datagrip
