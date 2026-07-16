11323019e08631df51c3332faa4561edf9f5ee3b
John Brosan <john.brosan@gmail.com>
2026-07-15 22:46:21 -0400

    Add Home Manager flake input and user configuration

---
+++ home/jbrosan/default.nix
@@ -0,0 +1,12 @@
+{ ... }:
+
+{
+  home.username = "jbrosan";
+  home.homeDirectory = "/home/jbrosan";
+
+  # Keep this at the version where Home Manager is first introduced.
+  home.stateVersion = "26.05";
+
+  programs.home-manager.enable = true;
+}
+
